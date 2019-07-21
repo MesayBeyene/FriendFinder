b1c3a304e6ba9ad6ba019d328b34ab91de592465
MesayBeyene <mesibella@gmail.com>
2019-07-20 21:46:29 -0700

    fourth commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,39 +6,42 @@ module.exports = function(app){
         res.json(friends);
       }); 
 
-      app.post('/api/friends', function (req, res) {
+     app.post('/api/friends', function (req, res) {
         
         var bestMatch = {
             name:"",
             photo:"",
             friendsDifference:1000
         };
-
         console.log(req.body);
         
-
-        // Here we take the result of the servay and parse it 
+        // Here we take the result of the servay and parse it.
         var userData = req.body;
         var userScores = userData.scores;
 
         console.log(userScores);
-
         var totalDifference = 0;
 
         // the loop through the friends in the DB
-        // for (var i = 0; i < friends.length; i++) {
-        //     console.log(friends[i]);
-        //     totalDifference = 0;
+        
+        for (var i = 0; i < friends.length; i++) {
+          console.log(friends[i]);
+          totalDifference = 0;
 
-        //     //loop through scores 
-        //     for (var j = 0; j < friends[i].scores[j]; j++) {
-        //         totalDifference += Math.abs(parseInt(userScores[j])- (parseInt(friends[i].scores[j]));
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
-        //         if (totalDifference <= bestMatch.friendsDifference);
-                
-        //         bestMatch.name = friends.name
-        //     }
-        // }
+                     if (totalDifference <= bestMatch.friendsDifference){
+                        bestMatch.name = friends[i].name;
+                        bestMatch.photo = friends[i].photo;
+                        bestMatch.friendDifference = totalDifference;
+                        } 
+                     }
+                    }
+                    friends.push(userData);
+                    console.log("New user added");
+                    console.log(userData);
+                    res.json(bestMatch);
       }); 
 }
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,10 @@ module.exports = function(app){
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
       }); 
-      
+      app.get("/friends", function(req, res) {
+        // res.sendFile(path.join(__dirname + "/../data/friends.js"));
+        res.json(friends)
+       });
       app.use(function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
       })
